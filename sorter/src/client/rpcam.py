5717678cccd82a0af97d5dbf7258e303602c8495
Finn <46852432+wurzelfels@users.noreply.github.com>
2020-05-17 14:48:12 +0200

    plt

--- sorter/src/client/rpcam.py
+++ sorter/src/client/rpcam.py
@@ -6,3 +6,7 @@ def showCamStream():
     camera.start_preview()
     sleep(5)
     camera.stop_preview()
+    
+    import matplotlib.pyplot as plt
+    plt.plot([-1, -4.5, 16, 23, 15, 59])
+    plt.show()
